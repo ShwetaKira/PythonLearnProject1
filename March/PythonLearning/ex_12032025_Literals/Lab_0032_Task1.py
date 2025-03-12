f577145f2c67aa334fae95bb09c3233e808d7e99
shwetakir <shweta.kiran46@gmail.com>
2025-03-12 15:39:16 +0100

    newchanges_12032025

--- March/PythonLearning/ex_12032025_Literals/Lab_0032_Task1.py
+++ March/PythonLearning/ex_12032025_Literals/Lab_0032_Task1.py
@@ -1,2 +1,16 @@
 #take 3 user inputs
 #perform sum , sub, mul and div
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+num3 = int(input("Enter num3: "))
+
+sum = num1 + num2 + num3
+sub = num1 - num2
+mul = num1 * num2 * num3
+div = num1 / num2
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
--- March/PythonLearning/ex_12032025_Literals/Lab_0033_Task2.py
+++ March/PythonLearning/ex_12032025_Literals/Lab_0033_Task2.py
@@ -3,3 +3,11 @@
 #num1 = 15 and num2 = 2
 # Q-. 7 and R -1
 
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+
+quotient = num1 //num2
+reminder = num1 % num2
+
+print(quotient)
+print(reminder)
