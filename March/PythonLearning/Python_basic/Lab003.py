8051dc5c4b0aeec65da7e7636399a6888677ad53
shwetakir <shweta.kiran46@gmail.com>
2025-03-12 10:54:28 +0100

    newchanges_1203

---
+++ March/PythonLearning/Python_basic/Lab003.py
@@ -0,0 +1,12 @@
+print("Hello world")
+print("Shweta")
+#print(self,*args,sep=' ', end ='\n',file= None)
+# *args - Unlimited number of comma separated arguments
+print("Shweta","123","Shweta","Shweta22")
+# seperator- is '-' which is not a default separator value
+print("Shweta","123","Shweta","Shweta22", sep ='-')
+print("Shweta","123","Shweta","Shweta", sep ='*')
+print("Shweta","123","Shweta","Shweta", sep ='|')
+# end= '_' instead of new line it takes _
+print("Shweta","123","Shweta","Shweta", sep ='*', end='     ')
+print("Shweta","123","Shweta","Shweta", sep ='-')
